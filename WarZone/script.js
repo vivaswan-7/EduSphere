554668ed84541ad206a19c33e70d925f45fa98d7
vivaswan-7 <aadijhaa30@gmail.com>
2025-03-08 01:58:28 +0530

    Update script.js

--- WarZone/script.js
+++ WarZone/script.js
@@ -71,3 +71,11 @@ document.addEventListener("DOMContentLoaded", function () {
     
 
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector("nav ul");
+
+    hamburger.addEventListener("click", function() {
+        navMenu.classList.toggle("active");
+    });
